61ed4ffe8efddf8ef18c5b1b4e1e4d689a3179ee
Utopik21 <maxime-bouton@outlook.com>
2019-12-18 08:41:56 +0100

    Suite itération de itération 3 avec les 3 colonnes

--- apps/schools/schools.py
+++ apps/schools/schools.py
@@ -6,12 +6,14 @@ import pandas as pd
 
 
 def get_datas_from_schools_csv(file_path):
-    data_frame = pd.read_csv(file_path, low_memory=False, sep=';', skipinitialspace=False)
+    data_frame = pd.read_csv(file_path, low_memory=False, sep=';', skipinitialspace=False, usecols=['Code commune','Taux Brut de Réussite Total séries','Taux_Mention_brut_toutes_series'])
+
+    print(data_frame.columns.tolist())
     return data_frame
 
 
 def merge_row_by_insee_code(data_frame):
-    data_frame_grouped = data_frame.groupby(['Code commune']).sum()
+    data_frame_grouped = data_frame.groupby(['Code commune']).mean()
 
     return data_frame_grouped
 
